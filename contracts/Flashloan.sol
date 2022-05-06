dcb6693c6ab29cf0a2f6138b748d424b569241b9
Cristy <cristi.gutzu@gmail.com>
2022-05-06 16:22:03 +0200

    utils

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -22,6 +22,16 @@ contract Flashloan is FlashLoanReceiverBase {
     address token1;
     address pairAddress0;
     address pairAddress1;
+
+
+    address _tokenPay;
+    address _tokenSwap;
+    uint256 _amountTokenPay;
+    address _sourceFactory;
+    address _targetFactory;
+    address _sourceRouter;
+    address _targetRouter;
+
     // end properties for swapping
 
     
@@ -29,38 +39,75 @@ contract Flashloan is FlashLoanReceiverBase {
             owner = msg.sender;
     }
 
-    function flashloanBnb(uint256 _amount) external  {
-        bytes memory data = "";
-
-        ILendingPool lendingPool = ILendingPool(
-            addressesProvider.getLendingPool()
-        );
-        // invoke a flashloan and receive a loan on this contract address
-        lendingPool.flashLoan(receiver, BNB_ADDRESS, _amount, data);
-    }
-
+    
     function executeOperation(
         address _reserve,
         uint256 _amount,
         uint256 _fee,
         bytes calldata _params
     ) external {
+
         require(
             _amount <= getBalanceInternal(address(this), _reserve),
             "Invalid balance, was the flashLoan successful?"
         );
 
-        //
-        // Your logic goes here.
-        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
-        //
+        /////////////////////////////////// swap logic here
+        uint256 deadline = getDeadline();
+        IERC20 TokenA = IERC20(token0);
+        IERC20 TokenB = IERC20(token1);
+        require(
+            TokenA.approve(address(exchangeA), _amount),
+            "Could not approve token0 sell"
+        );
+        address[] memory path;
+        if (token0 == WBNB || token1 == WBNB) {
+            path = new address[](2);
+            path[0] = token0;
+            path[1] = token1;
+        } else {
+            path = new address[](3);
+            path[0] = token0;
+            path[1] = WBNB;
+            path[2] = token1;
+        }
 
-        // IDefi app = IDefi(defi);
-        // Todo: Deposit into defi smart contract
-        // app.depositBNB.value(_amount)(_amount);
-        
-        // Todo: Withdraw from defi smart contract
-        // app.withdraw(_amount);
+        uint[] memory tokenBought;
+        tokenBought = IUniswapV2Router02(routerA).swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this), // or address(this), and transfer the swapped token to msg.sender
+            block.timestamp + 3
+        );
+
+        require(
+            TokenB.approve(address(exchangeB), tokenBought[0]),
+            "Could not approve token1 sell"
+        );
+
+        address[] memory path1;
+
+        if (token0 == WBNB || token1 == WBNB) {
+            path = new address[](2);
+            path[0] = token1;
+            path[1] = token0;
+        } else {
+            path = new address[](3);
+            path[0] = token1;
+            path[1] = WBNB;
+            path[2] = token0;
+        }
+        uint[] memory token1Bought;
+        token1Bought= IUniswapV2Router02(routerB).swapExactTokensForTokens(
+            tokenBought[0],
+            0,
+            path1,
+            address(this), // or address(this), and transfer the swapped token to msg.sender
+            block.timestamp + 3
+        );
+
+        /////////////////////////////////// swap ends here
 
         uint256 totalDebt = _amount.add(_fee);
         transferFundsBackToPoolInternal(_reserve, totalDebt);
@@ -69,4 +116,67 @@ contract Flashloan is FlashLoanReceiverBase {
     function rescueBNB(uint256 amount) external{
         payable(owner).transfer(amount);
     }
+
+    // This is the first function to call to execute arbitrage
+    function setParams(
+        address _tokenPay, // source currency when we will get; example BNB
+        address _tokenSwap, // swapped currency with the source currency; example BUSD
+        uint256 _amountTokenPay, // example: BNB => 10 * 1e18
+        address _targetFactory,
+        address _sourceRouter,
+        address _targetRouter) external {
+            _tokenPay = _tokenPay;
+            _tokenSwap = _tokenSwap;
+            _amountTokenPay = _amountTokenPay;
+            _sourceFactory = _sourceFactory;
+            _targetFactory = _targetFactory;
+            _sourceRouter = _sourceRouter;
+            _targetRouter = _targetRouter;
+    }
+    
+    // This is the last function to call to execute arbitrage
+    function executeArbi(){
+        if(_tokenPay==WBNB)
+        {
+            token0 = BNB_ADDRESS; 
+        } else{ 
+            token0 = _tokenPay; 
+        }
+
+        token1= _tokenSwap;
+
+        // recheck for stopping and gas usage
+
+        // https://docs.uniswap.org/protocol/V2/reference/smart-contracts/factory#getpair
+
+        require(
+            _tokenPay !=_tokenSwap,
+            "Same pairs "
+        ); 
+
+        pairAddress0 = IUniswapV2Factory(_sourceFactory).getPair(_tokenPay, _tokenSwap);
+        require(pairAddress0 != address(0), "first pair not available");
+        pairAddress1 = IUniswapV2Factory(_targetFactory).getPair(_tokenPay, _tokenSwap);
+        require(pairAddress0 != address(0), "second pair not available");
+
+
+        exchangeA = IUniswapV2Pair(pairAddress0);
+        exchangeB = IUniswapV2Pair(pairAddress1);
+
+
+        routerA= _sourceRouter;
+        routerB = _targetRouter;
+
+        bytes memory data = "";
+
+        ILendingPool lendingPool = ILendingPool(
+        addressesProvider.getLendingPool()
+        );
+
+
+        // invoke a flashloan and receive a loan on this contract address 
+        lendingPool.flashLoan(receiver, token0, _amountTokenPay, data);
+    }
+
+    
 }
