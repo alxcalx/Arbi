6e780ac8a933b9b5cf5827cebadf8b82db49f54b
Cristy <cristi.gutzu@gmail.com>
2022-05-06 15:46:37 +0200

    first demo flashloan and some interfaces

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -9,6 +9,26 @@ contract Flashloan is FlashLoanReceiverBase {
     address public receiver = address(this);
     address public constant BNB_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
+    address public owner;
+
+    // properties for swapping
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    //  address private constant pancakeRouter = 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F;
+    IUniswapV2Pair public exchangeA;
+    IUniswapV2Pair public exchangeB;
+    address routerA;
+    address routerB;
+    address token0;
+    address token1;
+    address pairAddress0;
+    address pairAddress1;
+    // end properties for swapping
+
+    
+	constructor() public {
+			owner = msg.sender;
+	}
+
     function flashloanBnb(uint256 _amount) external  {
         bytes memory data = "";
 
@@ -44,5 +64,9 @@ contract Flashloan is FlashLoanReceiverBase {
 
         uint256 totalDebt = _amount.add(_fee);
         transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+
+    function rescueBNB(uint256 amount) external{
+        payable(owner).transfer(amount);
     }
 }
